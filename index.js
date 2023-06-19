2455cbc23e89fc1d4ee22a819c4b88047b580c9a
22maksim <15maksim@mail.ru>
2023-06-19 18:43:42 +0300

    sort pasport

--- index.js
+++ index.js
@@ -1,36 +1,47 @@
-function getRandomNumberInRange(min, max) {
-  return Math.floor(Math.random() * (max - min)) + min;
-}
-
-const getWinner = (applicants, winnerObject) => {
-  const objKeys = Object.keys(winnerObject);
-  const num = getRandomNumberInRange(0, objKeys.length);
-  const links = Object.values(winnerObject);
-  const winner = links[num];
-  const out = {...applicants, ...winner};
-  
-  return out;
-};
-
-//---------callFunction--------
-const todaysWinner = {
-  prize: '10 000$',
-}
-
-const winnerApplicants = {
-  '001': {
-     name: 'Максим',
-     age: 25,
+const peopleWithVisa = [
+  {
+      firstName: 'Stasia',
+      lastName: 'Ward',
+      criminalRecord: true,
+      passportExpiration: '19.06.2040',
   },
-  '201': {
-     name: 'Светлана',
-     age: 20,
+  {
+      firstName: 'Elliot',
+      lastName: 'Baker',
+      criminalRecord: false,
+      passportExpiration: '04.06.2041',
   },
-  '304': {
-     name: 'Екатерина',
-     age: 35,
+  {
+      firstName: 'Leighann',
+      lastName: 'Scott',
+      criminalRecord: true,
+      passportExpiration: '31.07.2039',
   },
-}
+  {
+      firstName: 'Nick',
+      lastName: 'Pop',
+      criminalRecord: false,
+      passportExpiration: '31.12.2010',
+  },
+];
 
-const resultWinner = getWinner(todaysWinner, winnerApplicants);
-console.log('resultWinner', resultWinner);
+function allowVisa(withoutKillers) {
+  let actualDate = new Date();
+  actualDate = Date.parse(actualDate);
+  let withoutCriminal = withoutKillers.filter(item => item.criminalRecord !== true);
+  let answer = withoutCriminal.filter(item => {
+    let pars = item.passportExpiration.split('.');
+    pars = [pars[2], pars[1], pars[0]].join('-');
+    pars = new Date(pars);
+    pars = Date.parse(pars);
+    if (pars < actualDate) {
+      return pars
+    } else {
+      false
+    }
+  })
+  return answer;
+}
+ 
+const result = allowVisa(peopleWithVisa);
+console.log('result', result);
